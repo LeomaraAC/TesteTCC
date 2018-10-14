9046c9fb9134855215d1974aa715ffc6181057c4
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-14 01:33:37 -0300

    Classe responsável por devolver uma instância do WebDriver.

---
+++ src/test/java/until/DriverFactory.java
@@ -0,0 +1,49 @@
+package until;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+    private static ThreadLocal<WebDriver> threadDrive = new InheritableThreadLocal<WebDriver>(){
+        @Override
+        protected synchronized WebDriver initialValue(){
+            return initDrive();
+        }
+    };
+
+    private DriverFactory() {}
+
+    public static WebDriver getDriver() {
+        return threadDrive.get();
+    };
+
+    public static WebDriver initDrive() {
+        WebDriver driver = null;
+        switch (Propriedades.browser) {
+            case Chrome:
+                driver = new ChromeDriver();
+                break;
+            case Firefox:
+                driver = new FirefoxDriver();
+                break;
+            case Edge:
+                driver = new EdgeDriver();
+                break;
+        }
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static void killDriver(){
+        WebDriver driver = getDriver();
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+        if (threadDrive != null) {
+            threadDrive.remove();
+        }
+    }
+}
