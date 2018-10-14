ae9cbd50b7fca8e7933785afc9d0fd1404c2071a
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-14 01:32:37 -0300

    Arquivo com as propriedades do driver.

---
+++ src/test/java/until/Propriedades.java
@@ -0,0 +1,12 @@
+package until;
+
+public class Propriedades {
+    public static boolean FecharBrowser = true;
+    public static Browsers browser = Browsers.Chrome;
+    public static String linkPagina = "http://projetosara.meu";
+    public enum Browsers {
+        Chrome,
+        Firefox,
+        Edge
+    }
+}
