8be61c10a794b97e88d07d8fbf4a2cbc6dc729ef
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-14 01:36:38 -0300

    Classe contendo valores de variáveis que são utilizadas em diversos pontos da aplicação.

---
+++ src/test/java/until/Variables.java
@@ -0,0 +1,31 @@
+package until;
+
+public class Variables {
+    /***Login***/
+    public static  String login = "cv1002000";
+    public static  String senha = "secret";
+    public static String campoLogin = "//input[@name='prontuario']";
+    public static String campoSenha = "//input[@name='password']";
+    public static String btnEnviar = "//button[@type='submit']";
+    public static String linkDropdown = "//li[@class='nav-item dropdown']/a";
+    public static String linkSair = "//form[@id='logout-form']/a";
+
+    /***Menu***/
+    public static String menuAdministracao = "//a[.='Administração']";
+    public static String menuUsuario = "//a[.='Usuários']";
+    public static String menuAluno = "//a[.='Aluno']";
+    public static String menuAtendimento = "//a[.='Atendimento']";
+    public static String menuTipos = "//a[.='Tipos']";
+
+    /***Botões Novos***/
+    public static String btnNovo = "//a[@id='novo']";
+    public static String btnNovoTipo = "//a[@id='novoTipo']";
+
+    /***Xpath***/
+    public static String xpathMensagemSucesso = "//div[@class='v-snack__wrapper green']/div[@class='v-snack__content']/span";
+    public static String xpathMensagemErro = "//div[@class='v-snack__wrapper red']/div[@class='v-snack__content']/span";
+
+    /***IDs***/
+    public static String idBtnSalvar = "salvar";
+
+}
