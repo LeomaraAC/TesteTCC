3251915c4954ac7c6acd26ce2ba2ff6e1990f3b1
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-14 10:11:23 -0300

    Classe do pacote objects responsável por conter todas as variáveis que serão utilizadas na classe page do usuário.

---
+++ src/test/java/objects/UsuarioObject.java
@@ -0,0 +1,24 @@
+package objects;
+
+public class UsuarioObject {
+    //Dados
+    public static String nome = "Novo Useruário";
+    public static String prontuario = "cv12345";
+    public static String email = "novouser@yahoo.com";
+    public static String senhaUser = "secret";
+    public static String emailRepetido = "master@ifsp.com";
+
+    //IDs e Xpath
+    public static String idProntuario = "prontuario";
+    public static String idNome = "nome";
+    public static String idEmail = "email";
+    public static String idSenha = "senha";
+    public static String idSenhaConfirmation = "senha_confirmation";
+    public static String xpathMultiselect = "//div[@class='multiselect__select']";
+    public static String xpathCombo = "//*[@class='multiselect__content']//li[*='Master']";
+
+    //Mensagens sucesso
+    public static String msgSucesso = "Usuário criado com sucesso!";
+    public static String msgEmailRepetido = "O campo email já está sendo utilizado.";
+
+}
