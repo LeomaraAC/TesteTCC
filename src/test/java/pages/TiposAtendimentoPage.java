4bcf1b2ba89c4f9ac493a857144594886d78eda4
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-14 23:20:14 -0300

    Adicionando os testes de edição à classe de tipo de atendimento.

--- src/test/java/pages/TiposAtendimentoPage.java
+++ src/test/java/pages/TiposAtendimentoPage.java
@@ -149,4 +149,171 @@ public class TiposAtendimentoPage extends Base {
         }
 
     }
+
+    @Test
+    public void testEditarTipo() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca,descricaoEditar,idBtnFiltrar)
+                    .clicarCelula(idColunaEditar,colunaBusca,descricaoEditar,xpathTabela,xpathByClickEditar)
+                    .limpar(By.id(idDescricao))
+                    .escrever(idDescricao, descricaoEditarAlterado)
+                    .clicarBotao(idBtnSalvar)
+                    .assertMSG(xpathMensagemSucesso, msgSucessoEditar);
+            status = "OK";
+        }catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+
+    }
+
+    @Test
+    public void testEditarTipoVazio() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
+                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
+                    .clicarBotao(idBtnLimpar)
+                    .clicarBotao(idBtnSalvar)
+                    .assertMSG(xpathErroCampoDescricao, msgErroTipoVazio);
+            status = "OK";
+        }catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+
+    }
+
+    @Test
+    public void testEditarTipoRepetido() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
+                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
+                    .limpar(By.id(idDescricao))
+                    .escrever(idDescricao, descricaoExistenteRepetido)
+                    .clicarBotao(idBtnSalvar)
+                    .assertMSG(xpathMensagemErro, msgErroTipoRepetido);
+            status = "OK";
+        }catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+
+    }
+
+    @Test
+    public void testEditarTipoCaracteresEspeciais() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
+                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
+                    .limpar(By.id(idDescricao))
+                    .escrever(idDescricao, descricaoCaracteresEspeciais)
+                    .clicarBotao(idBtnSalvar)
+                    .assertMSG(xpathErroCampoDescricao, msgErroTipoCaracteresEspeciais);
+            status = "OK";
+        }catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+
+    }
+
+    @Test
+    public void testEditarTipoNumero() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
+                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
+                    .limpar(By.id(idDescricao))
+                    .escrever(idDescricao, descricaoNumeros)
+                    .clicarBotao(idBtnSalvar)
+                    .assertMSG(xpathErroCampoDescricao, msgErroTipoNumero);
+            status = "OK";
+        }catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+
+    }
+
+    @Test
+    public void testEditarTipoMinimoCaracteres() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
+                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
+                    .limpar(By.id(idDescricao))
+                    .escrever(idDescricao, descricaoMinimo)
+                    .clicarBotao(idBtnSalvar)
+                    .assertMSG(xpathErroCampoDescricao, msgErroTipoMinimo);
+            status = "OK";
+        }catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+
+    }
+
+    @Test
+    public void testExcluirTipo() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca,descricaoExcluir, idBtnFiltrar)
+                    .clicarCelula(idColunaExcluir,colunaBusca,descricaoExcluir,xpathTabela,xpathByClickExcluir)
+                    .assertMSG(xpathMensagemSucesso,msgSucessoExcluir);
+            status = "OK";
+        }catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+    }
+
+    @Test
+    public void testListaTipoInexistente() {
+        try {
+            menu.acessarTelaAtendimento();
+            new Steps()
+                    .filtrar(xpathBusca, descricaoInexistente, idBtnFiltrar)
+                    .esperar(600)
+                    .assertMSG(xpathMsgTabelaSemElemento, msgTabelaSemElemento);
+            status = "OK";
+        } catch (ComparisonFailure ex) {
+            status = "NOK";
+            error = ex.getMessage();
+        } catch (Exception ex) {
+            status = "FAIL";
+            error = ex.getMessage();
+        }
+    }
 }
