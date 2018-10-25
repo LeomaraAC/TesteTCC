28d83ba790067c248ee692e718242752c2840393
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-25 17:53:21 -0300

    Adicionando contains no xpath.

--- src/test/java/until/Variables.java
+++ src/test/java/until/Variables.java
@@ -4,26 +4,27 @@ public class Variables {
     /***Login***/
     public static  String login = "cv1002000";
     public static  String senha = "secret";
-    public static String campoLogin = "//input[@name='prontuario']";
-    public static String campoSenha = "//input[@name='password']";
-    public static String btnEnviar = "//button[@type='submit']";
-    public static String linkDropdown = "//li[@class='nav-item dropdown']/a";
-    public static String linkSair = "//form[@id='logout-form']/a";
+    ////input[contains(@id, 'senha')]
+    public static String campoLogin = "//input[contains(@name, 'prontuario')]";
+    public static String campoSenha = "//input[contains(@name, 'password')]";
+    public static String btnEnviar = "//button[contains(@type, 'submit')]";
+    public static String linkDropdown = "//li[contains(@class,'nav-item dropdown')]/a";
+    public static String linkSair = "//form[contains(@id, 'logout-form')]/a";
 
     /***Menu***/
-    public static String menuAdministracao = "//a[.='Administração']";
-    public static String menuUsuario = "//a[.='Usuários']";
-    public static String menuAluno = "//a[.='Aluno']";
-    public static String menuAtendimento = "//a[.='Atendimento']";
-    public static String menuTipos = "//a[.='Tipos']";
+    public static String menuAdministracao = "//a[contains(text(), 'Administração')]";
+    public static String menuUsuario = "//a[contains(text(), 'Usuários')]";
+    public static String menuAluno = "//a[contains(text(),'Aluno')]";
+    public static String menuAtendimento = "//a[contains(text(),'Atendimento')]";
+    public static String menuTipos = "//a[contains(text(),'Tipos')]";
 
     /***Botões Novos***/
-    public static String btnNovo = "//a[@id='novo']";
-    public static String btnNovoTipo = "//a[@id='novoTipo']";
+    public static String btnNovo = "//a[contains(@id, 'novo')]";
+    public static String btnNovoTipo = "//a[contains(@id, 'novoTipo')]";
 
     /***Xpath***/
-    public static String xpathMensagemSucesso = "//div[@class='v-snack__wrapper green']/div[@class='v-snack__content']/span";
-    public static String xpathMensagemErro = "//div[@class='v-snack__wrapper red']/div[@class='v-snack__content']/span";
+    public static String xpathMensagemSucesso = "//div[contains(@class, 'v-snack__wrapper green')]/div[contains(@class, 'v-snack__content')]/span";
+    public static String xpathMensagemErro = "//div[contains(@class, 'v-snack__wrapper red')]/div[contains(@class, 'v-snack__content')]/span";
 
     /***IDs***/
     public static String idBtnSalvar = "//button[contains(@id, 'salvar')]";
@@ -31,14 +32,14 @@ public class Variables {
     public static String idBtnFiltrar = "//div[contains(@id, 'btnInputFiltrar')]";
 
     //Xpath
-    public static String xpathBusca = "//div[@class='card-body']//input[@name='busca']";
+    public static String xpathBusca = "//div[contains(@class, 'card-body')]//input[contains(@name, 'busca')]";
 
     //Tabela
-    public static String xpathTabela = "//*[@id='tabelaListagem']//table";
+    public static String xpathTabela = "//*[contains(@id, 'tabelaListagem')]//table";
     public static int idColunaExcluir = 1;
     public static int idColunaEditar = 2;
     public static String xpathByClickExcluir = ".//i";
     public static String xpathByClickEditar = ".//a";
-    public static String xpathMsgTabelaSemElemento = "//*[@id='tabelaListagem']//table//tbody//div[@class='v-alert red']/div";
+    public static String xpathMsgTabelaSemElemento = "//*[contains(@id, 'tabelaListagem')]//table//tbody//div[contains(@class, 'v-alert red')]/div";
     public static String msgTabelaSemElemento = "Nenhum dado encontrado :(";
 }
