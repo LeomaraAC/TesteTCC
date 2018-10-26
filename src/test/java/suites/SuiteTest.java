80b3414009e4ab824a6ba852d6d220fd4a121a2c
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-26 00:58:33 -0300

    Adicionando o GrupoPage a suíte de teste.

--- src/test/java/suites/SuiteTest.java
+++ src/test/java/suites/SuiteTest.java
@@ -3,6 +3,7 @@ package suites;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import pages.GrupoPage;
 import pages.TiposAtendimentoPage;
 import pages.UsuarioPage;
 import until.DriverFactory;
@@ -10,7 +11,8 @@ import until.DriverFactory;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         UsuarioPage.class,
-        TiposAtendimentoPage.class
+        TiposAtendimentoPage.class,
+        GrupoPage.class
 })
 public class SuiteTest {
     @AfterClass
