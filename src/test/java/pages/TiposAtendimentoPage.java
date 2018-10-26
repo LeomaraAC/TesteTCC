0cf12421309c9aaf0c135874e6d35d60c4b590e3
“LeomaraACgit config --global user.name “LeomaraAC <leomara.andrade@gmail.com>
2018-10-26 00:57:14 -0300

    Remoção dos blocos de try catch.

--- src/test/java/pages/TiposAtendimentoPage.java
+++ src/test/java/pages/TiposAtendimentoPage.java
@@ -3,7 +3,6 @@ package pages;
 import core.Base;
 import core.Menu;
 import core.Steps;
-import org.junit.ComparisonFailure;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -15,295 +14,167 @@ public class TiposAtendimentoPage extends Base {
 
     @Test
     public void testInserirTipo() {
-        try {
-            menu.acessarTelaInserirTipo();
-            new Steps()
-                    .escrever(idDescricao, descricao)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemSucesso,msgSucessoIncluir);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+        menu.acessarTelaInserirTipo();
+        new Steps()
+                .escrever(idDescricao, descricao)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemSucesso, msgSucessoIncluir);
+        status = "OK";
 
     }
 
     @Test
     public void testInserirTipoRepetido() {
-        try {
-            menu.acessarTelaInserirTipo();
-            new Steps()
-                    .escrever(idDescricao, descricaoExistente)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemErro, msgErroTipoRepetido);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+        menu.acessarTelaInserirTipo();
+        new Steps()
+                .escrever(idDescricao, descricaoExistente)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemErro, msgErroTipoRepetido);
+        status = "OK";
     }
 
     @Test
     public void testInserirTipoVazio() {
-        try {
-            menu.acessarTelaInserirTipo();
-            new Steps()
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoVazio);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+        menu.acessarTelaInserirTipo();
+        new Steps()
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoVazio);
+        status = "OK";
     }
 
     @Test
     public void testInserirTipoCaracteresEspeciais() {
-        try {
-            menu.acessarTelaInserirTipo();
-            new Steps()
-                    .escrever(idDescricao, descricaoCaracteresEspeciais)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoCaracteresEspeciais);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+        menu.acessarTelaInserirTipo();
+        new Steps()
+                .escrever(idDescricao, descricaoCaracteresEspeciais)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoCaracteresEspeciais);
+        status = "OK";
     }
 
     @Test
     public void testInserirTipoNumero() {
-        try {
-            menu.acessarTelaInserirTipo();
-            new Steps()
-                    .escrever(idDescricao, descricaoNumeros)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoNumero);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+        menu.acessarTelaInserirTipo();
+        new Steps()
+                .escrever(idDescricao, descricaoNumeros)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoNumero);
+        status = "OK";
     }
 
     @Ignore
     public void testInserirTipoMaximoCaracteres() {
-        try {
-            menu.acessarTelaInserirTipo();
-            new Steps()
-                    .escrever(idDescricao, descricaoMaximo)
-                    .clicarBotao(idBtnSalvar);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+        menu.acessarTelaInserirTipo();
+        new Steps()
+                .escrever(idDescricao, descricaoMaximo)
+                .clicarBotao(idBtnSalvar);
+        status = "OK";
     }
 
     @Test
     public void testInserirTipoMinimoCaracteres() {
-        try {
-            menu.acessarTelaInserirTipo();
-            new Steps()
-                    .escrever(idDescricao, descricaoMinimo)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoMinimo);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+        menu.acessarTelaInserirTipo();
+        new Steps()
+                .escrever(idDescricao, descricaoMinimo)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoMinimo);
+        status = "OK";
     }
 
     @Test
-    public void testEditarTipo() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca,descricaoEditar,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,descricaoEditar,xpathTabela,xpathByClickEditar)
-                    .limpar(idDescricao)
-                    .escrever(idDescricao, descricaoEditarAlterado)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemSucesso, msgSucessoEditar);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarTipo() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoEditar, idBtnFiltrar)
+                .clicarCelula(idColunaEditar, colunaBusca, descricaoEditar, xpathTabela, xpathByClickEditar)
+                .limpar(idDescricao)
+                .escrever(idDescricao, descricaoEditarAlterado)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemSucesso, msgSucessoEditar);
+        status = "OK";
     }
 
     @Test
-    public void testEditarTipoVazio() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
-                    .clicarBotao(idBtnLimpar)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoVazio);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarTipoVazio() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoExistente, idBtnFiltrar)
+                .clicarCelula(idColunaEditar, colunaBusca, descricaoExistente, xpathTabela, xpathByClickEditar)
+                .clicarBotao(idBtnLimpar)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoVazio);
+        status = "OK";
     }
 
     @Test
-    public void testEditarTipoRepetido() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
-                    .limpar(idDescricao)
-                    .escrever(idDescricao, descricaoExistenteRepetido)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemErro, msgErroTipoRepetido);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarTipoRepetido() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoExistente, idBtnFiltrar)
+                .clicarCelula(idColunaEditar, colunaBusca, descricaoExistente, xpathTabela, xpathByClickEditar)
+                .limpar(idDescricao)
+                .escrever(idDescricao, descricaoExistenteRepetido)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemErro, msgErroTipoRepetido);
+        status = "OK";
     }
 
     @Test
-    public void testEditarTipoCaracteresEspeciais() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
-                    .limpar(idDescricao)
-                    .escrever(idDescricao, descricaoCaracteresEspeciais)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoCaracteresEspeciais);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarTipoCaracteresEspeciais() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoExistente, idBtnFiltrar)
+                .clicarCelula(idColunaEditar, colunaBusca, descricaoExistente, xpathTabela, xpathByClickEditar)
+                .limpar(idDescricao)
+                .escrever(idDescricao, descricaoCaracteresEspeciais)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoCaracteresEspeciais);
+        status = "OK";
     }
 
     @Test
-    public void testEditarTipoNumero() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
-                    .limpar(idDescricao)
-                    .escrever(idDescricao, descricaoNumeros)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoNumero);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarTipoNumero() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoExistente, idBtnFiltrar)
+                .clicarCelula(idColunaEditar, colunaBusca, descricaoExistente, xpathTabela, xpathByClickEditar)
+                .limpar(idDescricao)
+                .escrever(idDescricao, descricaoNumeros)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoNumero);
+        status = "OK";
     }
 
     @Test
-    public void testEditarTipoMinimoCaracteres() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca,descricaoExistente,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,descricaoExistente,xpathTabela,xpathByClickEditar)
-                    .limpar(idDescricao)
-                    .escrever(idDescricao, descricaoMinimo)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoDescricao, msgErroTipoMinimo);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarTipoMinimoCaracteres() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoExistente, idBtnFiltrar)
+                .clicarCelula(idColunaEditar, colunaBusca, descricaoExistente, xpathTabela, xpathByClickEditar)
+                .limpar(idDescricao)
+                .escrever(idDescricao, descricaoMinimo)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoDescricao, msgErroTipoMinimo);
+        status = "OK";
     }
 
     @Test
-    public void testExcluirTipo() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca,descricaoExcluir, idBtnFiltrar)
-                    .clicarCelula(idColunaExcluir,colunaBusca,descricaoExcluir,xpathTabela,xpathByClickExcluir)
-                    .assertMSG(xpathMensagemSucesso,msgSucessoExcluir);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+    public void testExcluirTipo() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoExcluir, idBtnFiltrar)
+                .clicarCelula(idColunaExcluir, colunaBusca, descricaoExcluir, xpathTabela, xpathByClickExcluir)
+                .assertMSG(xpathMensagemSucesso, msgSucessoExcluir);
+        status = "OK";
     }
 
     @Test
-    public void testListaTipoInexistente() {
-        try {
-            menu.acessarTelaAtendimento();
-            new Steps()
-                    .filtrar(xpathBusca, descricaoInexistente, idBtnFiltrar)
-                    .esperar(600)
-                    .assertMSG(xpathMsgTabelaSemElemento, msgTabelaSemElemento);
-            status = "OK";
-        } catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+    public void testListaTipoInexistente() throws InterruptedException {
+        menu.acessarTelaAtendimento();
+        new Steps()
+                .filtrar(xpathBusca, descricaoInexistente, idBtnFiltrar)
+                .esperar(600)
+                .assertMSG(xpathMsgTabelaSemElemento, msgTabelaSemElemento);
+        status = "OK";
     }
 }
--- src/test/java/pages/UsuarioPage.java
+++ src/test/java/pages/UsuarioPage.java
@@ -14,198 +14,121 @@ public class UsuarioPage extends Base {
 
     @Test
     public void testInserirUsuario() {
-        try {
-            menu.acessarTelaInserirUsuario();
-            new Steps()
-                    .escrever(idProntuario, prontuario)
-                    .escrever(idNome, nome)
-                    .escrever(idEmail, email)
-                    .clicarCombo(xpathMultiselect, xpathCombo)
-                    .escrever(idSenha, senhaUser)
-                    .escrever(idSenhaConfirmation, senhaUser)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemSucesso, msgSucessoIncluir);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+        menu.acessarTelaInserirUsuario();
+        new Steps()
+                .escrever(idProntuario, prontuario)
+                .escrever(idNome, nome)
+                .escrever(idEmail, email)
+                .clicarCombo(xpathMultiselect, xpathCombo)
+                .escrever(idSenha, senhaUser)
+                .escrever(idSenhaConfirmation, senhaUser)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemSucesso, msgSucessoIncluir);
+        status = "OK";
     }
 
     @Test
     public void testInserirComProntuarioRepetido() {
-        try {
-            menu.acessarTelaInserirUsuario();
-            new Steps()
-                    .escrever(idProntuario, prontuarioRepetido)
-                    .escrever(idNome, nome)
-                    .escrever(idEmail, email)
-                    .clicarCombo(xpathMultiselect, xpathCombo)
-                    .escrever(idSenha, senhaUser)
-                    .escrever(idSenhaConfirmation, senhaUser)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemErro,msgProntuarioRepetido);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+        menu.acessarTelaInserirUsuario();
+        new Steps()
+                .escrever(idProntuario, prontuarioRepetido)
+                .escrever(idNome, nome)
+                .escrever(idEmail, email)
+                .clicarCombo(xpathMultiselect, xpathCombo)
+                .escrever(idSenha, senhaUser)
+                .escrever(idSenhaConfirmation, senhaUser)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemErro,msgProntuarioRepetido);
+        status = "OK";
     }
 
     @Test
     public void testInserirComEmailRepetido() {
-        try {
-            menu.acessarTelaInserirUsuario();
-            new Steps()
-                    .escrever(idProntuario, prontuario)
-                    .escrever(idNome, nome)
-                    .escrever(idEmail, emailRepetido)
-                    .clicarCombo(xpathMultiselect, xpathCombo)
-                    .escrever(idSenha, senhaUser)
-                    .escrever(idSenhaConfirmation, senhaUser)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemErro, msgEmailRepetido);
-            status = "OK";
-        } catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "Fail";
-            getLog(ex);
-        }
+        menu.acessarTelaInserirUsuario();
+        new Steps()
+                .escrever(idProntuario, prontuario)
+                .escrever(idNome, nome)
+                .escrever(idEmail, emailRepetido)
+                .clicarCombo(xpathMultiselect, xpathCombo)
+                .escrever(idSenha, senhaUser)
+                .escrever(idSenhaConfirmation, senhaUser)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemErro, msgEmailRepetido);
+        status = "OK";
     }
 
 
     @Test
-    public void testEditarUsuarioNomeEmail() {
-        try {
-            menu.acessarTelaUsuario();
-            new Steps()
-                    .filtrar(xpathBusca,prontuarioEditar,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,prontuarioEditar,xpathTabela,xpathByClickEditar)
-                    .substituirValor(idNome, nomeEditadar)
-                    .substituirValor(idEmail, emailEditadar)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemSucesso, msgSucessoEditar);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarUsuarioNomeEmail() throws InterruptedException {
+        menu.acessarTelaUsuario();
+        new Steps()
+                .filtrar(xpathBusca,prontuarioEditar,idBtnFiltrar)
+                .clicarCelula(idColunaEditar,colunaBusca,prontuarioEditar,xpathTabela,xpathByClickEditar)
+                .substituirValor(idNome, nomeEditadar)
+                .substituirValor(idEmail, emailEditadar)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemSucesso, msgSucessoEditar);
+        status = "OK";
     }
     @Test
-    public void testEditarUsuarioSemGrupo() {
-        try {
-            menu.acessarTelaUsuario();
-            new Steps()
-                    .filtrar(xpathBusca,prontuarioAuxiliar,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,prontuarioAuxiliar,xpathTabela,xpathByClickEditar)
-                    .clicarCombo(xpathMultiselect, xpathCombo)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoGrupo, msgGrupoVazio);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testEditarUsuarioSemGrupo() throws InterruptedException {
+        menu.acessarTelaUsuario();
+        new Steps()
+                .filtrar(xpathBusca,prontuarioAuxiliar,idBtnFiltrar)
+                .clicarCelula(idColunaEditar,colunaBusca,prontuarioAuxiliar,xpathTabela,xpathByClickEditar)
+                .clicarCombo(xpathMultiselect, xpathCombo)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoGrupo, msgGrupoVazio);
+        status = "OK";
     }
 
     @Test
-    public void testEditarUsuarioVazio() {
-        try {
-            menu.acessarTelaUsuario();
-            new Steps()
-                    .filtrar(xpathBusca,prontuarioAuxiliar,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,prontuarioAuxiliar,xpathTabela,xpathByClickEditar)
-                    .clicarBotao(idBtnLimpar)
-                    .removerFocus(idProntuario)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoNome, msgNomeVazio)
-                    .assertMSG(xpathErroCampoProntuario, msgProntuarioVazio)
-                    .assertMSG(xpathErroCampoEmail, msgEmailVazio)
-                    .assertMSG(xpathErroCampoGrupo, msgGrupoVazio);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+    public void testEditarUsuarioVazio() throws InterruptedException {
+        menu.acessarTelaUsuario();
+        new Steps()
+                .filtrar(xpathBusca,prontuarioAuxiliar,idBtnFiltrar)
+                .clicarCelula(idColunaEditar,colunaBusca,prontuarioAuxiliar,xpathTabela,xpathByClickEditar)
+                .clicarBotao(idBtnLimpar)
+                .removerFocus(idProntuario)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoNome, msgNomeVazio)
+                .assertMSG(xpathErroCampoProntuario, msgProntuarioVazio)
+                .assertMSG(xpathErroCampoEmail, msgEmailVazio)
+                .assertMSG(xpathErroCampoGrupo, msgGrupoVazio);
+        status = "OK";
     }
 
     @Test
-    public void testEditarUsuarioSemProntuario() {
-        try {
-            menu.acessarTelaUsuario();
-            new Steps()
-                    .filtrar(xpathBusca,prontuarioEditar,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,prontuarioEditar,xpathTabela,xpathByClickEditar)
-                    .substituirValor(idProntuario, "")
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathErroCampoProntuario, msgProntuarioVazio);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+    public void testEditarUsuarioSemProntuario() throws InterruptedException {
+        menu.acessarTelaUsuario();
+        new Steps()
+                .filtrar(xpathBusca,prontuarioEditar,idBtnFiltrar)
+                .clicarCelula(idColunaEditar,colunaBusca,prontuarioEditar,xpathTabela,xpathByClickEditar)
+                .substituirValor(idProntuario, "")
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathErroCampoProntuario, msgProntuarioVazio);
+        status = "OK";
     }
 
     @Test
-    public void testEditarUsuarioProntuarioRepetido() {
-        try {
-            menu.acessarTelaUsuario();
-            new Steps()
-                    .filtrar(xpathBusca,prontuarioEditar,idBtnFiltrar)
-                    .clicarCelula(idColunaEditar,colunaBusca,prontuarioEditar,xpathTabela,xpathByClickEditar)
-                    .substituirValor(idProntuario, prontuarioRepetido)
-                    .clicarBotao(idBtnSalvar)
-                    .assertMSG(xpathMensagemErro, msgProntuarioRepetido);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
+    public void testEditarUsuarioProntuarioRepetido() throws InterruptedException {
+        menu.acessarTelaUsuario();
+        new Steps()
+                .filtrar(xpathBusca,prontuarioEditar,idBtnFiltrar)
+                .clicarCelula(idColunaEditar,colunaBusca,prontuarioEditar,xpathTabela,xpathByClickEditar)
+                .substituirValor(idProntuario, prontuarioRepetido)
+                .clicarBotao(idBtnSalvar)
+                .assertMSG(xpathMensagemErro, msgProntuarioRepetido);
+        status = "OK";
     }
 
     @Test
-    public void testExcluirUsuario() {
-        try {
-            menu.acessarTelaUsuario();
-            new Steps()
-                    .filtrar(xpathBusca,prontuarioExcluir, idBtnFiltrar)
-                    .clicarCelula(idColunaExcluir,colunaBusca,prontuarioExcluir,xpathTabela,xpathByClickExcluir)
-                    .assertMSG(xpathMensagemSucesso,msgSucessoExcluir);
-            status = "OK";
-        }catch (ComparisonFailure ex) {
-            status = "NOK";
-            getLog(ex);
-        } catch (Exception ex) {
-            status = "FAIL";
-            getLog(ex);
-        }
-
+    public void testExcluirUsuario() throws InterruptedException {
+        menu.acessarTelaUsuario();
+        new Steps()
+                .filtrar(xpathBusca,prontuarioExcluir, idBtnFiltrar)
+                .clicarCelula(idColunaExcluir,colunaBusca,prontuarioExcluir,xpathTabela,xpathByClickExcluir)
+                .assertMSG(xpathMensagemSucesso,msgSucessoExcluir);
+        status = "OK";
     }
 }
